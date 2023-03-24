f0f112dd9c07622157a0af1eec16a6acac823fe4
jonameijers <jonemeijers@gmail.com>
2023-03-24 10:28:05 +0000

    Human tool description to include action items

--- langchain/tools/human/tool.py
+++ langchain/tools/human/tool.py
@@ -18,8 +18,10 @@ class HumanInputRun(BaseTool):
     name = "Human"
     description = (
         "You can ask a human for guidance when you think you "
-        "got stuck or you are not sure what to do next. "
-        "The input should be a question for the human."
+        "got stuck or you are not sure what to do next."
+        "Engage the human to perform an action you cannot perform."
+        "The input should be a question or action item for the human."
+
     )
     prompt_func: Callable[[str], None] = Field(default_factory=lambda: _print_func)
     input_func: Callable = Field(default_factory=lambda: input)
